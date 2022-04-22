6b9885fa143b511d372789d04b8b8c429acfc2d6
EddYam <edder_yamir@hotmail.com>
2022-04-21 19:56:51 -0500

    4. Agregando clase de unidad para la clase User

---
+++ test/models/User.test.js
@@ -0,0 +1,13 @@
+describe("Unit Tests for User Class", () => {
+    test('Create an User object', () => {
+        const user = new User(1, "Eddyam", "Edder", "Bio", "dateCreated", "lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("Eddyam")
+        expect(user.name).toBe("Edder")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(usar.lastUpdated).toBe("lastUpdated")
+
+    })
+})
