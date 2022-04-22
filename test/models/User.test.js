5dd1f070595459e8383fb442d4716b2ebef20e8a
EddYam <edder_yamir@hotmail.com>
2022-04-22 00:05:00 -0500

    5. Agregando al archivo user.js la clase User e importandola

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,3 +1,5 @@
+const User = require("../../app/models/User")
+
 describe("Unit Tests for User Class", () => {
     test('Create an User object', () => {
         const user = new User(1, "Eddyam", "Edder", "Bio", "dateCreated", "lastUpdated")
