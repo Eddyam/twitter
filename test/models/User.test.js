6df6d7becbf1c6f9e07f4dacfd5048629d66a06b
EddYam <edder_yamir@hotmail.com>
2022-04-22 00:29:01 -0500

    8. Agregando pruebas para los getters de la clase User

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -10,6 +10,13 @@ describe("Unit Tests for User Class", () => {
         expect(user.bio).toBe("Bio")
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
+    })
 
+    test('Add getters', () => {
+        const user = new User(1, "Eddyam", "Edder", "Bio")
+        expect(user.getUsername).toBe("Eddyam")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
     })
 })
