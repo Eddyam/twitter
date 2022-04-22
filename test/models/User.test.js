fe786f001e404145f19e9c83d52a5227d446f877
EddYam <edder_yamir@hotmail.com>
2022-04-22 00:16:25 -0500

    6. Modificando prueba para no enviar valores dataCreated y lastUpdated y verificar que no sean undefined

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,14 +2,14 @@ const User = require("../../app/models/User")
 
 describe("Unit Tests for User Class", () => {
     test('Create an User object', () => {
-        const user = new User(1, "Eddyam", "Edder", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "Eddyam", "Edder", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("Eddyam")
         expect(user.name).toBe("Edder")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
 
     })
 })
