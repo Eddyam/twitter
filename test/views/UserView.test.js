8de362779d97948f96c9d53311252d33a3a63456
EddYam <edder_yamir@hotmail.com>
2022-04-24 15:16:31 -0500

    Sprint3-Paso 6. Agregando prueba para validar cuando solo se envie una de las propiedades

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,3 +1,4 @@
+const User = require("../../app/models/User")
 const UserView = require("../../app/views/UserView")
 
 describe("Test for UserView", () => {
@@ -9,6 +10,12 @@ describe("Test for UserView", () => {
 
     test("Return an error object when try to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
